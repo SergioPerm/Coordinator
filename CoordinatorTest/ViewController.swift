a192e89ba34c62ab8aedbc6fe5d95ee2c81ed7e1
SergioPerm <itotdel@list.ru>
2020-10-28 15:36:33 +0500

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by kluv on 28/10/2020.
+//  Copyright © 2020 itotdel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
